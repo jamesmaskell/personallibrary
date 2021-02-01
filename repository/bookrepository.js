ae9bb5534c9245c385e45613f09b65d8f484f2ef
James Maskell <jameslmaskell@gmail.com>
2021-02-01 20:06:14 +0000

    beta build

--- repository/bookrepository.js
+++ repository/bookrepository.js
@@ -1,59 +1,60 @@
 const ObjectId = require("mongodb").ObjectId;
 
 function BookRepository(client) {
-  this.addComment = async function (payload) {
-    try {
-      let result = await client
-        .db("personallibrary")
-        .collection("books")
-        .findOneAndUpdate({ _id: ObjectId(payload.id) }, { $push: { comments: payload.comment }, $inc: { commentcount: 1, __v: 1 } }, { returnOriginal: false });
-      if (result.value) return result.value;
-    } catch (e) {
-      console.error(e);
-    }
-  };
+	this.addComment = async function (payload) {
+		try {
+			let result = await client
+				.db("personallibrary")
+				.collection("books")
+				.findOneAndUpdate({ _id: ObjectId(payload.id) }, { $push: { comments: payload.comment }, $inc: { commentcount: 1, __v: 1 } }, { returnOriginal: false });
+			if (result.value) return result.value;
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-  this.delete = async function (bookId) {
-    try {
-      let result;
-      if (bookId == undefined) {
-        result = await client.db("personallibrary").collection("books").deleteMany({});
-      } else {
-        result = await client
-          .db("personallibrary")
-          .collection("books")
-          .deleteOne({ _id: ObjectId(bookId) });
-      }
-      if (result.deletedCount == undefined || result.deletedCount < 0) return false;
-      return true;
-    } catch (e) {
-      console.error(e);
-    }
-  };
+	this.delete = async function (bookId) {
+		try {
+			let result;
+			if (bookId == undefined) {
+				result = await client.db("personallibrary").collection("books").deleteMany({});
+				return true;
+			} else {
+				result = await client
+					.db("personallibrary")
+					.collection("books")
+					.deleteOne({ _id: ObjectId(bookId) });
+				if (result.deletedCount <= 0) return false;
+				return true;
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-  this.createBook = async function (payload) {
-    try {
-      let result = await client.db("personallibrary").collection("books").insertOne(payload);
-      return result.insertedId;
-    } catch (e) {
-      console.error(e);
-      1;
-    }
-  };
+	this.createBook = async function (payload) {
+		try {
+			let result = await client.db("personallibrary").collection("books").insertOne(payload);
+			return result.insertedId;
+		} catch (e) {
+			console.error(e);
+			1;
+		}
+	};
 
-  this.getBooks = async function (bookId) {
-    try {
-      if (bookId == undefined) {
-        return await client.db("personallibrary").collection("books").find({}).toArray();
-      }
-      return await client
-        .db("personallibrary")
-        .collection("books")
-        .findOne({ _id: ObjectId(bookId) });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+	this.getBooks = async function (bookId) {
+		try {
+			if (bookId == undefined) {
+				return await client.db("personallibrary").collection("books").find({}).toArray();
+			}
+			return await client
+				.db("personallibrary")
+				.collection("books")
+				.findOne({ _id: ObjectId(bookId) });
+		} catch (e) {
+			console.error(e);
+		}
+	};
 }
 
 module.exports = BookRepository;
